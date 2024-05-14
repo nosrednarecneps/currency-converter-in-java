2352a2c7ba54618c656e214c91941f9c02f2a5bc
theandersondev <spencyboy2007@gmail.com>
2024-05-14 12:48:37 -0600

    Added convertToInteger method

--- src/currencyConverter/Algorithms.java
+++ src/currencyConverter/Algorithms.java
@@ -1,8 +1,63 @@
 package currencyConverter;
 
-import java.util.ArrayList;
-
 public class Algorithms {
-    void insertion (ArrayList<Currency> arr) {
+    public char[] letters = {
+        'Z', 
+        'Y', 
+        'X', 
+        'W', 
+        'V',
+        'U',
+        'T',
+        'S',
+        'R',
+        'Q',
+        'P',
+        'O',
+        'N',
+        'M',
+        'L',
+        'Q',
+        'K',
+        'J',
+        'I',
+        'H',
+        'G',
+        'F',
+        'E',
+        'D',
+        'C',
+        'B',
+        'A'
+    };
+    int convertToInteger (char[] arr, String x) {
+        char[] characters = x.toCharArray(); 
+        String numbers = "";
+        for (int i = 0; i < characters.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] == characters[i]) {
+                    if (j >= 10) {
+                        numbers += "1" + j;
+                    } else {
+                        numbers += "10" + j;
+                    }
+                }
+            }
+        }
+        return Integer.valueOf(numbers);
+    }
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+        {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
