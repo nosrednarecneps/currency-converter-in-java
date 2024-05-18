1689cefee2e05b3ee2f6f10745cf8487b7573a3b
theandersondev <spencyboy2007@gmail.com>
2024-05-17 22:49:58 -0600

    2.3

--- src/currencyConverter/Currency.java
+++ src/currencyConverter/Currency.java
@@ -43,10 +43,34 @@ public class Currency {
 	public void setExchangeValues(String key, Double value) {
 		this.exchangeValues.put(key, value);
 	}
+<<<<<<< Updated upstream
+=======
+
+	public void convertBaseCurrency(String currency) {
+		Double usaConversion = 1 / this.exchangeValues.get(currency);
+		for (HashMap.Entry<String, Double> entry : this.exchangeValues.entrySet()) {
+			String key = entry.getKey();
+			Double value = entry.getValue();
+			this.exchangeValues.put(key, usaConversion * value);
+		}
+	}
+
+	public void putValues() {
+		this.exchangeValues.put("USD", 1.00);
+		this.exchangeValues.put("EUR", 0.93);
+		this.exchangeValues.put("GBP", 0.66);
+		this.exchangeValues.put("CHF", 1.01);
+		this.exchangeValues.put("CNY", 6.36);
+		this.exchangeValues.put("JPY", 123.54);
+		this.exchangeValues.put("CAD", 1.37);	
+		orderCurrencies();
+	}
+>>>>>>> Stashed changes
 	
 	// Set default values for a currency
 	public void defaultValues() {
 		String currency = this.name;
+<<<<<<< Updated upstream
 		
 		switch (currency) {	
 			case "US Dollar":
@@ -56,6 +80,11 @@ public class Currency {
 				this.exchangeValues.put("CHF", 1.01);
 				this.exchangeValues.put("CNY", 6.36);
 				this.exchangeValues.put("JPY", 123.54);
+=======
+		putValues();	
+		switch (currency) {	
+			case "US Dollar":
+>>>>>>> Stashed changes
 				break;
 			case "Euro":
 				this.exchangeValues.put("USD", 1.073);
@@ -101,7 +130,7 @@ public class Currency {
 	}
 	
 	// Initialize currencies
-	public static ArrayList<Currency> init() {
+	public void declareCurrencies() {
 		ArrayList<Currency> currencies = new ArrayList<Currency>();
 				
 		currencies.add( new Currency("US Dollar", "USD") );
@@ -110,6 +139,7 @@ public class Currency {
 		currencies.add( new Currency("Swiss Franc", "CHF") );
 		currencies.add( new Currency("Chinese Yuan Renminbi", "CNY") );
 		currencies.add( new Currency("Japanese Yen", "JPY") );
+<<<<<<< Updated upstream
 		
 		for (Integer i =0; i < currencies.size(); i++) {
 			currencies.get(i).defaultValues();
@@ -118,6 +148,44 @@ public class Currency {
 		return currencies;
 	}
 	
+=======
+		currencies.add( new Currency("Canadian Dollar", "CAD"));
+						//currencies.get(i).defaultValues();
+	}
+
+    public void iterateUsingForEach(ArrayList<String> arr, HashMap <String, Double> map) {
+		Iterator iterator = map.entrySet().iterator();
+		Integer i = 0;
+    	while (iterator.hasNext()) {
+			HashMap.Entry entry = (HashMap.Entry)iterator.next();
+			String key = ((String)entry.getKey());
+			arr.add(key);
+			i++;
+    	}
+	}
+
+	public void getShortNamesAsArray(String[] arr) {
+		arr = this.shortNames.toArray(arr);
+	}
+
+	public void orderCurrencies() {
+		iterateUsingForEach(this.shortNames, this.exchangeValues);
+		String[] shortNames = new String[this.shortNames.size()];
+		Integer[] integerShortNames = new Integer[this.shortNames.size()];
+		char[] letters = algorithmsObject.getLetters();
+		getShortNamesAsArray(shortNames);
+		for (int i = 0; i < shortNames.length; i++) {
+			integerShortNames[i] = algorithmsObject.convertToInteger(letters, shortNames[i]);
+		}					
+		algorithmsObject.sort(integerShortNames);
+		for (int i = 0; i < integerShortNames.length; i++) {
+			String convertedShortName = algorithmsObject.convertToCurrencies(letters, integerShortNames[i]);
+			this.shortNames.set(i, convertedShortName);
+		}
+
+	}
+
+>>>>>>> Stashed changes
 	// Convert a currency to another
 	public static Double convert(Double amount, Double exchangeValue) {
 		Double price;
